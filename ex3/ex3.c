b4b15148f332eda3c2ea5636cfb81c1cda250386
DeepakSuryaS <ideepaksuryas@gmail.com>
2018-07-15 19:10:47 +0530

    Done.

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -7,6 +7,8 @@ int main(int argc, char ** argv) {
 	
 	printf("I am %d years old.\n", age);
 	printf("I am %d inches tall.\n", height);
+	printf("Alright, let's count, say \a1, \a2, \a3, \a4,... I'm tired let's just say \a10.\n"); //the \a is an escape sequence for a beep or alarm
+	printf("This is the percent sign - %%.\n"); //using the % format specifier to display the percent sign
 	
 	return 0;
 }
